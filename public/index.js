3fae65887b8c68e3aec0586e080716f37b5f4d38
Luke Barrier <lukebbarrier06@outlook.com>
2024-11-21 20:49:33 -0500

    Update

--- public/index.js
+++ public/index.js
@@ -1,1 +1,17 @@
 // Your code here 
+
+const URL = 'https://api.thecatapi.com/v1/images/search';
+
+window.onload = async () => {
+    console.log('here');
+    const image = document.getElementById('image');
+
+    const response = await fetch(URL);
+    const [data] = await response.json();
+
+    const { url } = data;
+
+    console.log(url);
+
+    image.src = url;
+};
